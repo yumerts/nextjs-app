dbea465ec23ac40f50c3d72b2368b9d56ed76c01
Victor Mak <contact@htetaung.com>
2024-11-17 06:06:06 +0700

    feat: fixed wrong prediction contract

--- app/helpers/payusdc.tsx
+++ app/helpers/payusdc.tsx
@@ -10,26 +10,26 @@ export const pay_usdc_to_prediction_contract = async (wallet: ConnectedWallet, m
     const signer = provider.getSigner();
   
     const USDC_TOKEN_ADDRESS = "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d"
-    const PREDICTION_SMART_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_PREDICTION_SMART_CONTRACT_ADDRESS ?? ""
+    const PREDICTION_SMART_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_PREDICTION_SMART_CONTRACT_ADDRESS as `0x${string}` ?? "0x0000000000000000000000000000000000000000"
   
     const USDC_TOKEN_APPROVE_ABI = erc20Abi;
   
     const USDC_FUNCTION_DATA = encodeFunctionData({
       abi: USDC_TOKEN_APPROVE_ABI, 
       functionName: "approve",
-      args: [USDC_TOKEN_ADDRESS, BigInt(usdc_amount)]
+      args: [`${PREDICTION_SMART_CONTRACT_ADDRESS}`, BigInt(usdc_amount)]
     });
     const USDC_APPROVAL_REQUEST = {
       to: USDC_TOKEN_ADDRESS,
       data: USDC_FUNCTION_DATA
     }
   
-    signer.sendTransaction(USDC_APPROVAL_REQUEST);
+    await signer.sendTransaction(USDC_APPROVAL_REQUEST);
   
     const PREDICT_FUNCTION_DATA = encodeFunctionData({
       abi: prediction_contract_abi,
       functionName: "predictMatch",
-      args: [Number(match_id), Number(chosen_player), Number(usdc_amount)]
+      args: [match_id, chosen_player, BigInt(usdc_amount)]
     })
   
     const PREDICTION_REQUEST = {
@@ -37,7 +37,7 @@ export const pay_usdc_to_prediction_contract = async (wallet: ConnectedWallet, m
       data: PREDICT_FUNCTION_DATA
     }
   
-    signer.sendTransaction(PREDICTION_REQUEST);
+    await signer.sendTransaction(PREDICTION_REQUEST);
 
     return;
   }
